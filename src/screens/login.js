3a968116fbb183f34b20aa5467bc2592dbcec969
Zakaria Kerkeb <z.kerkeb@gmail.com>
2022-12-05 12:14:39 +0100

    exo 2

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,15 +1,79 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import styled from 'styled-components';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({navigation}) => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  //fonction pour récuperer un token
+  const logMeIn = async () => {
+    if (password.length < 8) {
+      alert('Password must be at least 8 characters long');
+      return;
+    }
+
+    axios({
+      method: 'post',
+      url: 'https://login.hikkary.com/users/login',
+      data: {
+        username: username,
+        password: password,
+      },
+    })
+      .then(async response => {
+        console.log(response.headers['x-access-token']);
+        await AsyncStorage.setItem('token', response.headers['x-access-token']);
+        navigation.navigate('Characters');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <View>
       <Text>Login</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Characters')}>
-        <Text>TO Characters</Text>
-      </TouchableOpacity>
+      <View>
+        <TextInputContainer>
+          <TextInputStyled
+            value={username}
+            onChangeText={text => setUsername(text)}
+          />
+        </TextInputContainer>
+        <TextInputContainer>
+          <TextInputStyled
+            value={password}
+            onChangeText={text => setPassword(text)}
+            secureTextEntry={true}
+          />
+        </TextInputContainer>
+        <LoginButton onPress={logMeIn}>
+          <Text>Login</Text>
+        </LoginButton>
+      </View>
     </View>
   );
 };
+
+//Utilisation des styled components
+const LoginButton = styled.TouchableOpacity`
+  background-color: red;
+  padding: 12px;
+  border-radius: 4px;
+`;
+
+const TextInputContainer = styled.View`
+  margin: 10px;
+`;
+
+const TextInputStyled = styled.TextInput`
+  background-color: red;
+  padding: 12px;
+  border-radius: 12px;
+  color: white;
+`;
 
 export default Login;
